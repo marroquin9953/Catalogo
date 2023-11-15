76cb60ab1d0b8238215ef4c8729984ca404390a7
Isidro Marroquin <106924754+marroquin9953@users.noreply.github.com>
2023-11-15 12:13:24 -0600

    Add confi

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,7 +5,7 @@ import { ReactComponent as IconGithub } from '../../assets/github.svg'
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <p>Proyecto realizado por <a href='https://github.com/marroquin9953' target='_blank' rel='noreferrer'> Isidro Marroquin <IconGithub /></a></p>
+      <p>©Tecniservicios H Dos Mil S.A DE C.V |<a href='https://github.com/marroquin9953' target='_blank' rel='noreferrer'> Todos los derechos reservados.<IconGithub /></a></p>
     </footer>
   )
 }
